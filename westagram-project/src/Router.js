f9a527301c7b0896938f0ec43f47a63b71ce1050
hdh <ghdengus12@naver.com>
2022-03-16 16:20:24 +0900

    ADD : initail setting complete

---
+++ westagram-project/src/Router.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Router = () => {
+  return <div>화면 나오나 ?</div>;
+};
+
+export default Router;
---
+++ westagram-project/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/reset.scss';
+import './styles/common.scss';
+import Router from './Router';
+
+ReactDOM.render(<Router />, document.getElementById('root'));
