8561eecdff8e28c9d9a64f73b0f10f4c400dfe3a
jay yoon <jaytaildish@gmail.com>
2022-03-17 17:52:31 +0900

    MOD:Main, Aside SCSS수정완료

--- src/pages/jaewoong/Main/Aside/Aside.js
+++ src/pages/jaewoong/Main/Aside/Aside.js
@@ -1,208 +1,179 @@
 import React from 'react';
+import './Aside.scss';
 
 const Aside = () => {
   return (
     <aside>
-      <div className="personalContainer">
-        <div className="userContainer">
-          <img
-            className="userThumbnail"
-            alt="유져썸네일"
-            src="../../../../images/jaewoong/westa-contents/first-contents.png"
-          />
-          <div className="userInfo doubleLineContainer">
-            <a className="userid smallBoldFont" href="">
+      <div className="userContainer">
+        <img
+          className="myThumbnail"
+          alt="유져썸네일"
+          src="../../../../images/jaewoong/westa-contents/first-contents.png"
+        />
+        <dl>
+          <dt>
+            <a className="userid bold" href="www">
               jay yoon
             </a>
-            <div>
-              <span>윤재웅</span>
-            </div>
-          </div>
-        </div>
-        <div className="asideBox">
-          <div className="asideBoxHeader">
-            <span className="asideBoxTitle">스토리</span>
-            <a className="asideBoxSeeAllBtn" href="">
-              모두보기
-            </a>
-          </div>
-          <div className="userstodyList">
-            <ul>
-              <li className="asideBoxList">
-                <div className="thumbnailIdContainer">
-                  <img
-                    className="no1 middleThumbnail"
-                    alt="팔로워썸네일"
-                    src="../../../../images/jaewoong/westa-userthumnails/thumb1.png"
-                  />
-                  <div className="doubleLineContainer">
-                    <span className="no1 recommandId smallBoldFont">
-                      아이디1
-                    </span>
-                    <span className="no1 friendsOfMineLocation smallGrayFont">
-                      3분전
-                    </span>
-                  </div>
-                </div>
-              </li>
-              <li className="asideBoxList">
-                <div className="thumbnailIdContainer">
-                  <img
-                    className="no1 middleThumbnail"
-                    alt="팔로워썸네일"
-                    src="../../../../images/jaewoong/westa-userthumnails/thumb2.png"
-                  />
-                  <div className="doubleLineContainer">
-                    <span className="no1 recommandId smallBoldFont">
-                      아이디2
-                    </span>
-                    <span className="no1 friendsOfMineLocation smallGrayFont">
-                      10분전
-                    </span>
-                  </div>
-                </div>
-              </li>
-              <li className="asideBoxList">
-                <div className="thumbnailIdContainer">
-                  <img
-                    className="no1 middleThumbnail"
-                    alt="팔로워썸네일"
-                    src="../../../../images/jaewoong/westa-userthumnails/thumb3.png"
-                  />
-                  <div className="doubleLineContainer">
-                    <span className="no1 recommandId smallBoldFont">
-                      아이디3
-                    </span>
-                    <span className="no1 friendsOfMineLocation smallGrayFont">
-                      2일전
-                    </span>
-                  </div>
-                </div>
-              </li>
-              <li className="asideBoxList">
-                <div className="thumbnailIdContainer">
-                  <img
-                    className="no1 middleThumbnail"
-                    alt="팔로워썸네일"
-                    src="../../../../images/jaewoong/westa-userthumnails/thumb4.png"
-                  />
-                  <div className="doubleLineContainer">
-                    <span className="no1 recommandId smallBoldFont">
-                      아이디4
-                    </span>
-                    <span className="no1 friendsOfMineLocation smallGrayFont">
-                      3일전
-                    </span>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="asideBox recommandContainer">
-          <div className="asideBoxHeader">
-            <span className="asideBoxTitle">회원님을 위한 추천</span>
-            <a className="asideBoxSeeAllBtn" href="">
-              모두보기
-            </a>
-          </div>
-          <div className="recommandList">
-            <ul>
-              <li className="asideBoxList">
-                <div className="thumbnailIdContainer">
-                  <img
-                    className="no1 middleThumbnail"
-                    alt="팔로워썸네일"
-                    src="../../../../images/jaewoong/westa-userthumnails/thumb5.png"
-                  />
-                  <div className="doubleLineContainer">
-                    <span className="no1 recommandId smallBoldFont">
-                      아이디5
-                    </span>
-                    <span className="no1 friendsOfMineLocation smallGrayFont">
-                      aaa외 13명이 팔로우하고있습니다.
-                    </span>
-                  </div>
-                </div>
-                <span className="followBtn">팔로우</span>
-              </li>
-              <li className="asideBoxList">
-                <div className="thumbnailIdContainer">
-                  <img
-                    className="no1 middleThumbnail"
-                    alt="팔로워썸네일"
-                    src="../../../../images/jaewoong/westa-userthumnails/thumb6.png"
-                  />
-                  <div className="doubleLineContainer">
-                    <span className="no1 recommandId smallBoldFont">
-                      아이디6
-                    </span>
-                    <span className="no1 friendsOfMineLocation smallGrayFont">
-                      aaa외 13명이 팔로우하고있습니다.
-                    </span>
-                  </div>
-                </div>
-                <span className="followBtn">팔로우</span>
-              </li>
-              <li className="asideBoxList">
-                <div className="thumbnailIdContainer">
-                  <img
-                    className="no1 middleThumbnail"
-                    alt="팔로워썸네일"
-                    src="../../../../images/jaewoong/westa-userthumnails/thumb1.png"
-                  />
-                  <div className="doubleLineContainer">
-                    <span className="no1 recommandId smallBoldFont">
-                      아이디7
-                    </span>
-                    <span className="no1 friendsOfMineLocation smallGrayFont">
-                      aaa외 13명이 팔로우하고있습니다.
-                    </span>
-                  </div>
-                </div>
-                <span className="followBtn">팔로우</span>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="footer">
-          <ul>
-            <li>
-              <a href="">instagram 정보</a>
-            </li>
-            <li>
-              <a href="">지원</a>
-            </li>
-            <li>
-              <a href="">홍보센터</a>
-            </li>
-            <li>
-              <a href="">API</a>
-            </li>
-            <li>
-              <a href="">채용 정보</a>
-            </li>
-            <li>
-              <a href="">개인정보처리방침</a>
-            </li>
-            <li>
-              <a href="">약관</a>
-            </li>
-            <li>
-              <a href="">디렉터리</a>
-            </li>
-            <li>
-              <a href="">프로필</a>
-            </li>
-            <li>
-              <a href="">해시태그</a>
-            </li>
-            <li>
-              <a href="">언어</a>
-            </li>
-          </ul>
-          <div>2019 WESTAGRAM</div>
-        </div>
+          </dt>
+          <dd>윤재웅</dd>
+        </dl>
+      </div>
+      <div className="asideBoxHeader">
+        <span className="asideBoxTitle gray">스토리</span>
+        <a className="asideBoxSeeAllBtn" href="www">
+          모두보기
+        </a>
+      </div>
+      <div className="asideBox story">
+        <ul>
+          <li className="asideBoxList">
+            <img
+              className="no1 middleThumbnail"
+              alt="팔로워썸네일"
+              src="../../../../images/jaewoong/westa-userthumnails/thumb1.png"
+            />
+            <dl className="doubleLineContainer">
+              <dt className="no1 bold">아이디1</dt>
+              <dd className="no1 gray">3분전</dd>
+            </dl>
+          </li>
+          <li className="asideBoxList">
+            <img
+              className="no1 middleThumbnail"
+              alt="팔로워썸네일"
+              src="../../../../images/jaewoong/westa-userthumnails/thumb2.png"
+            />
+            <dl className="doubleLineContainer">
+              <dt className="no1 recommandId bold">아이디2</dt>
+              <dd className="no1 friendsOfMineLocation gray">10분전</dd>
+            </dl>
+          </li>
+          <li className="asideBoxList">
+            <img
+              className="no1 middleThumbnail"
+              alt="팔로워썸네일"
+              src="../../../../images/jaewoong/westa-userthumnails/thumb3.png"
+            />
+            <dl className="doubleLineContainer">
+              <dt className="no1 recommandId bold">아이디3</dt>
+              <dd className="no1 friendsOfMineLocation gray">2일전</dd>
+            </dl>
+          </li>
+          <li className="asideBoxList">
+            <img
+              className="no1 middleThumbnail"
+              alt="팔로워썸네일"
+              src="../../../../images/jaewoong/westa-userthumnails/thumb4.png"
+            />
+            <dl className="doubleLineContainer">
+              <dt className="no1 recommandId bold">아이디4</dt>
+              <dd className="no1 friendsOfMineLocation gray">3일전</dd>
+            </dl>
+          </li>
+        </ul>
+      </div>
+      <div className="asideBoxHeader">
+        <span className="asideBoxTitle gray">회원님을 위한 추천</span>
+        <a className="asideBoxSeeAllBtn" href="www">
+          모두보기
+        </a>
+      </div>
+      <div className="asideBox recommand">
+        <ul>
+          <li className="asideBoxList">
+            <img
+              className="no1 middleThumbnail"
+              alt="팔로워썸네일"
+              src="../../../../images/jaewoong/westa-userthumnails/thumb5.png"
+            />
+            <dl>
+              <dt className="bold">아이디5</dt>
+              <dd className="gray">aaa외 13명이 팔로우하고있습니다.</dd>
+            </dl>
+            <span className="mainBlue">팔로우</span>
+          </li>
+          <li className="asideBoxList">
+            <img
+              className="no1 middleThumbnail"
+              alt="팔로워썸네일"
+              src="../../../../images/jaewoong/westa-userthumnails/thumb6.png"
+            />
+            <dl className="doubleLineContainer">
+              <dt className="no1 recommandId bold">아이디6</dt>
+              <dd className="no1 friendsOfMineLocation gray">
+                aaa외 13명이 팔로우하고있습니다.
+              </dd>
+            </dl>
+            <span className="mainBlue">팔로우</span>
+          </li>
+          <li className="asideBoxList">
+            <img
+              className="no1 middleThumbnail"
+              alt="팔로워썸네일"
+              src="../../../../images/jaewoong/westa-userthumnails/thumb1.png"
+            />
+            <dl className="doubleLineContainer">
+              <dt className="no1 recommandId bold">아이디7</dt>
+              <dd className="no1 friendsOfMineLocation gray">
+                aaa외 13명이 팔로우하고있습니다.
+              </dd>
+            </dl>
+            <span className="mainBlue">팔로우</span>
+          </li>
+          <li className="asideBoxList">
+            <img
+              className="no1 middleThumbnail"
+              alt="팔로워썸네일"
+              src="../../../../images/jaewoong/westa-userthumnails/thumb1.png"
+            />
+            <dl className="doubleLineContainer">
+              <dt className="no1 recommandId bold">아이디7</dt>
+              <dd className="no1 friendsOfMineLocation gray">
+                aaa외 13명이 팔로우하고있습니다.
+              </dd>
+            </dl>
+            <span className="mainBlue">팔로우</span>
+          </li>
+        </ul>
+      </div>
+      <div className="footer">
+        <ul>
+          <li>
+            <a href="w">instagram 정보</a>
+          </li>
+          <li>
+            <a href="w">지원</a>
+          </li>
+          <li>
+            <a href="w">홍보센터</a>
+          </li>
+          <li>
+            <a href="w">API</a>
+          </li>
+          <li>
+            <a href="w">채용 정보</a>
+          </li>
+          <li>
+            <a href="w">개인정보처리방침</a>
+          </li>
+          <li>
+            <a href="w">약관</a>
+          </li>
+          <li>
+            <a href="w">디렉터리</a>
+          </li>
+          <li>
+            <a href="w">프로필</a>
+          </li>
+          <li>
+            <a href="w">해시태그</a>
+          </li>
+          <li>
+            <a href="w">언어</a>
+          </li>
+        </ul>
+        <div>2019 WESTAGRAM</div>
       </div>
     </aside>
   );
--- src/pages/jaewoong/Main/Main.js
+++ src/pages/jaewoong/Main/Main.js
@@ -1,96 +1,93 @@
 import React from 'react';
 import Nav from '../../../components/Nav/Nav';
 import Aside from './Aside/Aside';
+import './Main.scss';
 
 const Main = () => {
   return (
     <>
       <Nav />
-      <div id="mainPage">
+      <main>
         <Aside />
-        <main className="main">
-          <section className="section">
-            <article className="no1 postContainer">
-              <div className="postTopbar">
-                <img
-                  className="middleThumbnail"
-                  alt="팔로워 썸네일"
-                  src="../../../../images/jaewoong/westa-contents/first-contents.png"
-                />
-                <div className="doubleLineContainer">
-                  <span className="followeeId smallBoldFont">jay yoon</span>
-                  <span className="followeeLocation smallGrayFont">
-                    location스탠딩바 전기
-                  </span>
-                </div>
-                <img
-                  className="tridotBtn"
-                  alt="게시글 설정"
-                  src="../../../../images/jaewoong/dot_btn.png"
-                />
-              </div>
+        <section>
+          <article>
+            <div className="postHeader">
               <img
-                className="photo"
-                alt="사진"
+                className="middleThumbnail"
+                alt="팔로워 썸네일"
                 src="../../../../images/jaewoong/westa-contents/first-contents.png"
               />
-              <div className="postInteractionParts">
-                <div className="postIconBar">
-                  <div className="postIconContainer">
-                    <img
-                      className="postIcon blankHeart"
-                      alt="blank heart"
-                      src="../../../../images/jaewoong/heart_logo.png"
-                    />
-                    <img
-                      className="postIcon redHeart hidden"
-                      alt="red heart"
-                      src="../../../../images/jaewoong/redheart.png"
-                    />
-                    <img
-                      className="postIcon reply"
-                      alt="reply"
-                      src="../../../../images/jaewoong/talk.png"
-                    />
-                    <img
-                      className="postIcon download"
-                      alt="download"
-                      src="../../../../images/jaewoong/download.png"
-                    />
-                  </div>
+              <dl>
+                <dt className="bold">jay yoon</dt>
+                <dd className="gray">스탠딩바 전기</dd>
+              </dl>
+              <img
+                className="tridotBtn"
+                alt="게시글 설정"
+                src="../../../../images/jaewoong/dot_btn.png"
+              />
+            </div>
+            <img
+              className="photo"
+              alt="사진"
+              src="../../../../images/jaewoong/westa-contents/first-contents.png"
+            />
+            <div className="postInteractionParts">
+              <div className="postIconBar">
+                <div className="postIconContainer">
                   <img
-                    className="postIcon bookmark"
-                    alt="bookmart"
-                    src="../../../../images/jaewoong/bookmark.png"
+                    className="postIcon blankHeart"
+                    alt="blank heart"
+                    src="../../../../images/jaewoong/heart_logo.png"
                   />
-                </div>
-                <div className="postLikeCounter">
                   <img
-                    className="smallFolloweeThumbnail"
-                    alt="thumbnail"
-                    src="../../../../images/jaewoong/westa-userthumnails/thumb5.png"
+                    className="postIcon redHeart hidden"
+                    alt="red heart"
+                    src="../../../../images/jaewoong/redheart.png"
                   />
-                  <span className="smallBoldFont">aineworld 외 10명</span>
-                  <span className="small-font">이 좋아합니다.</span>
-                </div>
-                <div className="postMaintext">
-                  <span className="smallBoldFont">jay yoon</span>
-                  <span>아 또가고싶다 또가면되지</span>
-                </div>
-                <ul className="replyContainer" />
-                <form className="replyComponent">
-                  <input
-                    className="insertReply"
-                    type="text"
-                    placeholder="댓글 달기..."
+                  <img
+                    className="postIcon reply"
+                    alt="reply"
+                    src="../../../../images/jaewoong/talk.png"
                   />
-                  <button className="submitReply">입력</button>
-                </form>
+                  <img
+                    className="postIcon download"
+                    alt="download"
+                    src="../../../../images/jaewoong/download.png"
+                  />
+                </div>
+                <img
+                  className="postIcon bookmark"
+                  alt="bookmart"
+                  src="../../../../images/jaewoong/bookmark.png"
+                />
               </div>
-            </article>
-          </section>
-        </main>
-      </div>
+              <p>
+                <img
+                  className="smallFolloweeThumbnail"
+                  alt="thumbnail"
+                  src="../../../../images/jaewoong/westa-userthumnails/thumb5.png"
+                />
+                <span className="bold">aineworld 외 10명</span>
+                <span className="small-font">이 좋아합니다.</span>
+              </p>
+              <dl className="postMaintext">
+                <dt className="bold">jay yoon</dt>
+                <dd>아 또가고싶다 또가면되지</dd>
+              </dl>
+              <ul className="replyContainer" />
+              <form className="replyComponent">
+                <input
+                  className="insertReply"
+                  type="text"
+                  placeholder="댓글 달기..."
+                />
+                <button className="submitReply">입력</button>
+              </form>
+            </div>
+          </article>
+        </section>
+      </main>
     </>
   );
 };
