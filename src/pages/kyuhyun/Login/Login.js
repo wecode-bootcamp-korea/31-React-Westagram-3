69aa880efa09e88c5b3fd908e00e3108a0d2ab9f
kyuhyun ro <kyuhyunro@kyuhyunui-MacBookPro.local>
2022-03-17 14:19:08 +0900

    [31기 노규현]ADD: First Commit

--- src/pages/kyuhyun/Login/Login.js
+++ src/pages/kyuhyun/Login/Login.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import './Login.scss';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-  return <div>노규현 로그인</div>;
+  const navigate = useNavigate();
+  const goToMain = () => {
+    navigate('/main');
+  };
+
+  return (
+    <div className="Login">
+      <div className="main">
+        <div className="logo">
+          <header className="logoText">westagram</header>
+        </div>
+
+        <div className="idPw">
+          <input
+            className="id"
+            tpye="text"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+          />
+          <input className="pw" tpye="text" placeholder="비밀번호" />
+        </div>
+        <input
+          type="button"
+          className="loginBtn"
+          value="로그인"
+          onClick={goToMain}
+        />
+        <div className="lostPw">
+          <span>비밀번호를 잊으셨나요?</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/kyuhyun/Main/Main.js
+++ src/pages/kyuhyun/Main/Main.js
@@ -1,7 +1,76 @@
 import React from 'react';
+import './Main.scss';
+import Aside from './Aside/Aside';
 
 const Main = () => {
-  return <div>노규현 메인</div>;
+  return (
+    <div className="Main">
+      <main className="page">
+        <div className="main">
+          <div className="feeds">
+            <article className="article">
+              <section className="articleTop">
+                <div className="articleTopLeft">
+                  <div className="articleTopLeftImg">
+                    <img src="/images/kyuhyun/storyimg1.png" />
+                  </div>
+                  <h5 className="articleTopLeftText">sunnyfterrain</h5>
+                </div>
+
+                <div className="articleTopRight">
+                  <i className="fa-solid fa-ellipsis" />
+                </div>
+              </section>
+              <div className="articleMiddle">
+                <img src="/images/kyuhyun/profile2.png" />
+              </div>
+
+              <footer className="articleBottom">
+                <div className="articleBottomTop">
+                  <div className="articleBottomTopLeft">
+                    <div className="redHeart">
+                      <i className="fa-solid fa-heart" />
+                    </div>
+                    <i className="fa-regular fa-comment" />
+                    <i className="fa-solid fa-arrow-up-from-bracket" />
+                  </div>
+                  <div className="articleBottomTopRight">
+                    <i className="fa-regular fa-bookmark" />
+                  </div>
+                </div>
+                <div className="articleBottomMiddle">
+                  <div className="articleBottomMiddleLike">
+                    <img src="/images/kyuhyun/my.png" />
+                    <span className="articleBottomMiddleText">
+                      loubxxtin님 외 10명이 좋아합니다.
+                    </span>
+                  </div>
+                </div>
+                <div className="articleBottomMiddleComment">
+                  <div className="articleBottomMiddleCommentText" />
+                </div>
+                <form className="articleBottomBottom">
+                  <input
+                    type="text"
+                    className="articleBottomBottomComment"
+                    placeholder="댓글 달기.."
+                  />
+                  <button
+                    type="button"
+                    className="articleBottomBottomCommentBtn"
+                  >
+                    게시
+                  </button>
+                </form>
+              </footer>
+            </article>
+          </div>
+        </div>
+        {/* 메인우측시작 */}
+        <Aside />
+      </main>
+    </div>
+  );
 };
 
 export default Main;
