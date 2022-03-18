08899d7d9522994c7ff7c3770cabd6576da045ac
kyuhyun ro <kyuhyunro@kyuhyunui-MacBookPro.local>
2022-03-18 18:01:21 +0900

    ADD:로그인 유효성 검사 기능 추가

--- src/pages/kyuhyun/Login/Login.js
+++ src/pages/kyuhyun/Login/Login.js
@@ -1,10 +1,28 @@
 import './Login.scss';
 import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
 
 const Login = () => {
   const navigate = useNavigate();
   const goToMain = () => {
-    navigate('/main');
+    navigate('/main-kyuhyun');
+  };
+
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+
+  const handleIdInput = event => {
+    setId(event.target.value);
+  };
+
+  const handlePwInput = event => {
+    setPw(event.target.value);
+  };
+
+  const isValueTrue = () => {
+    id.includes('@') && pw.length > 5
+      ? goToMain()
+      : alert('아이디, 비밀번호를 확인하세요.');
   };
 
   return (
@@ -16,17 +34,24 @@ const Login = () => {
 
         <div className="idPw">
           <input
+            onChange={handleIdInput}
             className="id"
             tpye="text"
             placeholder="전화번호, 사용자 이름 또는 이메일"
           />
-          <input className="pw" tpye="text" placeholder="비밀번호" />
+          <input
+            onChange={handlePwInput}
+            className="pw"
+            tpye="text"
+            placeholder="비밀번호"
+            // onChange={this.handleChage}
+          />
         </div>
         <input
           type="button"
           className="loginBtn"
           value="로그인"
-          onClick={goToMain}
+          onClick={isValueTrue}
         />
         <div className="lostPw">
           <span>비밀번호를 잊으셨나요?</span>
