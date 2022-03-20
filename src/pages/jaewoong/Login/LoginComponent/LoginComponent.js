e9c7aa6746971b5cf7c4b4669ebd7a3bd2167975
jaydvps <jaytaildish@gmail.com>
2022-03-21 00:33:44 +0900

    ADD:[31기 윤재웅]로그인버튼 활성화 기능구현(onchange한칸씩 레이턴시발생)

--- src/pages/jaewoong/Login/LoginComponent/LoginComponent.js
+++ src/pages/jaewoong/Login/LoginComponent/LoginComponent.js
@@ -5,72 +5,74 @@ import { useNavigate } from 'react-router-dom';
 import './LoginComponent.scss';
 
 const LoginComponent = () => {
-  // const navigate = useNavigate();
-
   const [username, setUsername] = useState();
   const [pwd, setPwd] = useState();
-  const [usernameValidation, setUsernameValidation] = useState(false);
-  const [pwdValidation, setPwdValidation] = useState(false);
+  const [inputValidation, setInputValidation] = useState(false);
+
   const navigate = useNavigate();
 
-  const handleInput = e => {
-    setUsername(e.target.value);
-    setPwd(e.target.value);
-    console.log(`ID:${username} / pwd"${pwd}`);
+  const handleIdInput = e => {
+    const target = e.target;
+    setUsername(target.value);
+    checkUserValidation();
   };
 
-  const checkUsername = id => {
-    if (id.indexOf('@') < 0) return false;
-    else {
-      return true;
-    }
+  const handlePwdInput = e => {
+    const target = e.target;
+    setPwd(target.value);
+    checkUserValidation();
   };
 
-  const checkPwd = pwd => {
-    if (pwd.length < 5) return false;
-    else return true;
+  const checkUserValidation = () => {
+    const checkUsername = username => {
+      if (username.indexOf('@') == -1) return false;
+      else return true;
+    };
+
+    const checkPwd = pwd => {
+      if (pwd.length < 5) return false;
+      else return true;
+    };
+
+    checkUsername(username) && checkPwd(pwd)
+      ? setInputValidation(true)
+      : setInputValidation(false);
   };
 
-  const handleClick = () => {
-    usernameValidation && pwdValidation
+  const handleSubmit = () => {
+    inputValidation
       ? navigate('/main-jaewoong', {
           state: {
-            username: usernameValidation,
+            username: username,
           },
         })
       : alert('다시 입력해주세요');
   };
 
   return (
-    <form
-      className="loginForm"
-      onSubmit={() => {
-        handleClick();
-      }}
-    >
+    <form className="loginForm" onSubmit={handleSubmit}>
       <input
         className="username"
         placeholder="전화번호,사용자 이름 또는 이메일"
-        // onChange={e => {
-        //   setUsernameValidation(checkUsername(e.target.value));
-        // }}
-        onChange={handleInput}
+        type="email"
+        onChange={handleIdInput}
       />
       <input
         className="pwd"
         type="password"
         placeholder="비밀번호"
-        // onChange={e => {
-        //   setPwdValidation(checkPwd(e.target.value));
-        // }}
-        onChange={handleInput}
+        minLength={5}
+        onChange={handlePwdInput}
       />
       {/*<Link to="/main-jaewoong">
         <button className="loginBtn" onClick={checkValidation}>
           Login
         </button>
       </Link>*/}
-      <button className="loginBtn" type="submit">
+      <button
+        className={inputValidation ? 'btnActivate' : 'loginBtn'}
+        type="submit"
+      >
         login
       </button>
     </form>
