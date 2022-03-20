0a779f6a7ff5e97f187721e433127025bde5b6d5
jay yoon <jaytaildish@gmail.com>
2022-03-20 21:27:59 +0900

    ADD:ID/PWD유효성검사 기능 개발

--- src/pages/jaewoong/Login/LoginComponent/LoginComponent.js
+++ src/pages/jaewoong/Login/LoginComponent/LoginComponent.js
@@ -1,84 +1,68 @@
-import React, { Component, uesRef } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import './LoginComponent.scss';
 
-const username = document.querySelector('.username');
-const pwd = document.querySelector('.pwd');
-const loginBtn = document.querySelector('.loginBtn');
-const BTN_ACTIVATE = 'btnActivate';
+const LoginComponent = () => {
+  // const navigate = useNavigate();
 
-const checkId = () => {
-  console.log('함수진입');
-  if (username.value.indexOf('@') == -1) return false;
-  else return true;
-};
-
-const checkPwd = () => {
-  if (pwd.value.length < 5) return false;
-  else return true;
-};
-
-function LoginComponent() {
+  const [usernameValidation, setUsernameValidation] = useState(false);
+  const [pwdValidation, setPwdValidation] = useState(false);
   const navigate = useNavigate();
 
+  const checkUsername = username => {
+    if (username.indexOf('@') < 0) return false;
+    else {
+      return true;
+    }
+  };
+
+  const checkPwd = pwd => {
+    if (pwd.length < 5) return false;
+    else return true;
+  };
+
+  const handleClick = () => {
+    usernameValidation && pwdValidation
+      ? navigate('/main-jaewoong', {
+          state: {
+            username: usernameValidation,
+          },
+        })
+      : alert('다시 입력해주세요');
+  };
+
   return (
-    <form className="loginForm">
+    <form
+      className="loginForm"
+      onSubmit={() => {
+        handleClick();
+      }}
+    >
       <input
         className="username"
-        type="email"
         placeholder="전화번호,사용자 이름 또는 이메일"
+        onChange={e => {
+          setUsernameValidation(checkUsername(e.target.value));
+        }}
       />
       <input
         className="pwd"
         type="password"
-        minLength="5"
         placeholder="비밀번호"
+        onChange={e => {
+          setPwdValidation(checkPwd(e.target.value));
+        }}
       />
-      <Link to="/main-jaewoong">
-        <button className="loginBtn">Login</button>
-      </Link>
-      {/*<button className="loginBtn" onClick={()=>{
-        checkId() && checkPwd()
-        ? console.log("로그인성공")
-        : alert("사용자이름에는 @이 포함되어야 하며 비밀번호는 5글자 이d상 입력해주셔야 합니다.")
-      }}>login</button>*/}
+      {/*<Link to="/main-jaewoong">
+        <button className="loginBtn" onClick={checkValidation}>
+          Login
+        </button>
+      </Link>*/}
+      <button className="loginBtn" type="submit">
+        login
+      </button>
     </form>
   );
-}
-
+};
 export default LoginComponent;
-
-// const loginBtnActivate=()=>{
-//   loginBtn.classList.add(BTN_ACTIVATE);
-//   loginBtn.disabled=false;
-// }
-
-// const loginBtnDectivate=()=>{
-//   loginBtn.classList.remove(BTN_ACTIVATE);
-//   loginBtn.disabled=true;
-// }
-
-// const btnColorHandler = (e) =>{
-//   e.value=username.value;
-//   username.value && pwd.value
-//   ? loginBtnActivate()
-//   : loginBtnDectivate()
-// }
-
-// const loginSuccess=()=>{
-//   localStorage.setItem("ID",username.value);
-//   window.location ="main.html"
-// }
-
-// const loginInfoHandler =()=>{
-//   checkId() && checkPwd()
-//   ? loginSuccess()
-//   : alert("사용자이름에는 @이 포함되어야 하며 비밀번호는 5글자 이상 입력해주셔야 합니다.")
-// }
-
-// const init=()=>{
-//   username.addEventListener("input",btnColorHandler);
-//   pwd.addEventListener("input",btnColorHandler);
-//   loginBtn.addEventListener("click", loginInfoHandler);
-// }
