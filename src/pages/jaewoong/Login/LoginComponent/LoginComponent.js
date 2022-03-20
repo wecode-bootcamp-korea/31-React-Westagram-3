da31cda4acca7ba18ab19d3bfdde7604bddca865
jaydvps <jaytaildish@gmail.com>
2022-03-20 22:49:32 +0900

    ADD:[31기 윤재웅]Mission1-사용자입력데이터 저장기능구현

--- src/pages/jaewoong/Login/LoginComponent/LoginComponent.js
+++ src/pages/jaewoong/Login/LoginComponent/LoginComponent.js
@@ -1,3 +1,4 @@
+import { isValidDateValue } from '@testing-library/user-event/dist/utils';
 import React, { useState } from 'react';
 // import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
@@ -6,12 +7,20 @@ import './LoginComponent.scss';
 const LoginComponent = () => {
   // const navigate = useNavigate();
 
+  const [username, setUsername] = useState();
+  const [pwd, setPwd] = useState();
   const [usernameValidation, setUsernameValidation] = useState(false);
   const [pwdValidation, setPwdValidation] = useState(false);
   const navigate = useNavigate();
 
-  const checkUsername = username => {
-    if (username.indexOf('@') < 0) return false;
+  const handleInput = e => {
+    setUsername(e.target.value);
+    setPwd(e.target.value);
+    console.log(`ID:${username} / pwd"${pwd}`);
+  };
+
+  const checkUsername = id => {
+    if (id.indexOf('@') < 0) return false;
     else {
       return true;
     }
@@ -42,17 +51,19 @@ const LoginComponent = () => {
       <input
         className="username"
         placeholder="전화번호,사용자 이름 또는 이메일"
-        onChange={e => {
-          setUsernameValidation(checkUsername(e.target.value));
-        }}
+        // onChange={e => {
+        //   setUsernameValidation(checkUsername(e.target.value));
+        // }}
+        onChange={handleInput}
       />
       <input
         className="pwd"
         type="password"
         placeholder="비밀번호"
-        onChange={e => {
-          setPwdValidation(checkPwd(e.target.value));
-        }}
+        // onChange={e => {
+        //   setPwdValidation(checkPwd(e.target.value));
+        // }}
+        onChange={handleInput}
       />
       {/*<Link to="/main-jaewoong">
         <button className="loginBtn" onClick={checkValidation}>
