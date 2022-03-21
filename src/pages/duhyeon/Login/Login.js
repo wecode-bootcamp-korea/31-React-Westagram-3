4b6cbc0560431e1c017c6143462d9947f2224374
hdh <ghdengus12@naver.com>
2022-03-21 21:42:36 +0900

    modify: useState 객체 방식으로 리팩토링 및 메인페이지 이동 기능 추가

--- src/pages/duhyeon/Login/Login.js
+++ src/pages/duhyeon/Login/Login.js
@@ -1,53 +1,56 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../Login/Login.scss';
 
 const Login = () => {
-  const [validId, setValidId] = useState('');
-  const [validPw, setValidPw] = useState('');
-  const [isActive, setIsActive] = useState(false);
-
-  const handleIdInput = e => {
-    setValidId(e.target.value);
-    isPassedLogin();
+  const [inputs, setInputs] = useState({
+    id: '',
+    password: '',
+  });
+  const onChange = e => {
+    setInputs(inputs => ({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    }));
   };
-
-  const handlePwInput = e => {
-    setValidPw(e.target.value);
-    isPassedLogin();
+  let idValid =
+    inputs.id.includes('@') && inputs.password.length > 4 ? true : false;
+  const navigate = useNavigate();
+  const goToMain = () => {
+    navigate('/main-duhyeon');
   };
-
-  const isPassedLogin = () => {
-    validId.includes('@') && validPw.length > 4
-      ? setIsActive(true)
-      : setIsActive(false);
-  };
-
   return (
     <div className="container">
       <div className="login">
         <h1 className="logo">westagram</h1>
         <div className="login-form">
-          <input
-            type="text"
-            id="user_id"
-            onChange={handleIdInput}
-            autoComplete="off"
-            placeholder="전화번호,사용자 이름 또는 이메일"
-          />
-          <input
-            type="password"
-            id="user_pw"
-            onChange={handlePwInput}
-            autoComplete="off"
-            placeholder="비밀번호"
-          />
-          <button
-            type="button"
-            className={isActive ? 'btn-login active' : 'btn-login'}
-            disabled={isActive ? true : false}
-          >
-            로그인
-          </button>
+          <form>
+            <input
+              name="id"
+              value={inputs.id}
+              type="text"
+              onChange={onChange}
+              autoComplete="off"
+              placeholder="전화번호,사용자 이름 또는 이메일"
+            />
+            <input
+              name="password"
+              value={inputs.password}
+              type="password"
+              onChange={onChange}
+              onKeyDown={goToMain}
+              autoComplete="off"
+              placeholder="비밀번호"
+            />
+            <button
+              type="button"
+              className={idValid ? 'btn-login active' : 'btn-login'}
+              disabled={idValid ? false : true}
+              onClick={goToMain}
+            >
+              로그인
+            </button>
+          </form>
         </div>
         <p className="find-password">비밀번호를 잊으셨나요?</p>
       </div>
